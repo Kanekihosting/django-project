453dd5f4200abfc8eddd017591fb6523c6f2c64d
Kirito <sasskee0507t@gmail.com>
2024-04-24 23:42:18 +0500

    commit1

--- config/settings.py
+++ config/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    str(BASE_DIR.joinpath('static'))
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
